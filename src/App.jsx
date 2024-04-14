08fed911bdcd69d8137b12237b64f801cd8cd350
Gefila <106391467+Gefila@users.noreply.github.com>
2024-04-14 23:07:26 +0700

    add & delete todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,29 @@ import { useEffect, useState } from "react";
 import Task from "./components/Task";
 
 function App() {
-	const [todo, setTodo] = useState([
-	]);
+	const [todo, setTodo] = useState([]);
 
-	let [task, setTask] = useState("")
+	let [task, setTask] = useState("");
 
+	function handleAddTodo() {
+		setTodo([...todo, { id: +new Date(), task: task, status: false }]);
+		setTask("");
+	}
 
-
+	function handleDeleteTodo(todoId) {
+		setTodo(todo.filter((e) => e.id !== todoId));
+	}
 
 	return (
 		<div className="bg-slate-800 w-full h-screen flex flex-col items-center">
 			<div className="bg-sky-400 flex flex-col px-7 py-1 items-center w-2/6  mt-4 rounded">
 				<div className="py-4">TO DO LIST</div>
-				<form action="">
-					
-				</form>
+				<form action=""></form>
 				<input
 					type="text"
 					className="p-2 w-full rounded-t"
 					placeholder="Example : Olahraga"
-					onChange={(e)=> setTask(e.target.value)}
+					onChange={(e) => setTask(e.target.value)}
 					value={task}
 				/>
 				<button
@@ -32,7 +35,10 @@ function App() {
 				</button>
 				<div className="flex flex-col w-full">
 					{todo.map((todo) => (
-						<Task key={todo.id} handleDelete={()=> handleDeleteTodo(todo.id)}>{`${todo.task} | ${todo.id}`}</Task>
+						<Task
+							key={todo.id}
+							handleDelete={() => handleDeleteTodo(todo.id)}
+						>{`${todo.task} | ${todo.id}`}</Task>
 					))}
 				</div>
 			</div>
